433701c19d5a9f1dd91aa51c820947f924b20ce2
yerayherreria <yerayherre@gmail.com>
2023-12-13 14:22:33 +0100

    subiendo

--- src/main/java/com/jacaranda/calculator/controller/ControllerCalculator.java
+++ src/main/java/com/jacaranda/calculator/controller/ControllerCalculator.java
@@ -2,10 +2,15 @@ package com.jacaranda.calculator.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jacaranda.calculator.model.Calculator;
+import com.jacaranda.calculator.model.ElectricalPower;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class ControllerCalculator {
@@ -28,5 +33,36 @@ public class ControllerCalculator {
 		model.addAttribute("result", result);
 		return "calculadora";
 		
+	}
+	
+	@GetMapping("/electricalPower")
+	public String electricalPower(Model model) {
+		ElectricalPower ep = new ElectricalPower();
+		model.addAttribute("elp",ep);
+		
+		return "electricalPower";
+	}
+	
+	@GetMapping("/resol")
+	public String resol(Model model, @ModelAttribute("elp") ElectricalPower ep) throws Exception {
+		ElectricalPower ep2 = new ElectricalPower();
+		model.addAttribute("elp",ep2);
+		String result = "Electrical Power: "+ep.getVolt()+"*"+ep.getAmp()+"="+ep.getResult()+"W";
+		
+		model.addAttribute("result", result);
+		return "electricalPower";
+		
+	}
+	
+	@GetMapping("/error")
+	public String error(Model model, @RequestParam("msg") String msg) {
+		model.addAttribute("msg", msg);
+		return "error";
+	}
+
+	@ExceptionHandler(Exception.class)
+	public String handleException(Exception e, HttpServletRequest request) {
+	    // Handle it
+	    return "error?msg="+ e.getMessage();
 	}
 }
---
+++ src/main/java/com/jacaranda/calculator/model/ElectricalPower.java
@@ -0,0 +1,52 @@
+package com.jacaranda.calculator.model;
+
+import java.util.Objects;
+
+public class ElectricalPower {
+	private int volt;
+	private int amp;
+	
+	public int getVolt() {
+		return volt;
+	}
+	public void setVolt(int volt) throws Exception {
+		try {
+			this.volt = volt;			
+		}catch(Exception e) {
+			throw new Exception("Volt not valid");
+		}
+	}
+	public int getAmp() {
+		return amp;
+	}
+	public void setAmp(int amp) throws Exception {
+		try {
+			this.amp = amp;
+			
+		}catch(Exception e) {
+			throw new Exception("Amp not valid");
+		}
+	}
+	
+	public double getResult() {
+		return this.volt*this.amp;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(amp, volt);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ElectricalPower other = (ElectricalPower) obj;
+		return amp == other.amp && volt == other.volt;
+	}
+	
+	
+}
